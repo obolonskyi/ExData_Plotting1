04f404188d6e934959ff37e5b3fa5c94593b12ea
obolonskyi <m.obolonskyi@gmail.com>
2017-07-17 10:36:12 +0300

    Update Plot4.R

--- Plot4.R
+++ Plot4.R
@@ -15,6 +15,8 @@ Datetime1 <- as.POSIXct(Datetime, format="%d/%m/%Y %H:%M:%S", tz="")
 
 
 ## create plot4
+png(filename = "plot4.png",width = 480, height = 480)
+
 par(mfrow=c(2,2), pty="s")
 plot(Datetime1, newepc$Global_active_power, type="l", col="black", xlab="", ylab="Global Active Power")
 plot(Datetime1, newepc$Voltage, type="l", col="black", xlab="datetime", ylab="Voltage")
@@ -24,5 +26,5 @@ lines(Datetime1, newepc$Sub_metering_3, type="l", col="blue")
 legend("topright", legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), col=c("black","red","blue"), lty=1, cex=0.6, bty="n")
 plot(Datetime1, newepc$Global_reactive_power, type="l", col="black", xlab="datetime", ylab="Global_reactive_power")
 
-png(filename = "plot4.png",width = 480, height = 480)
+
 dev.off()
